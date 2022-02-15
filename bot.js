7ed0b10046e50dba50c7bede197cd168947f351c
AibSyed <asyed1447@gmail.com>
2022-02-14 23:30:42 -0600

    remove recursive function call

--- bot.js
+++ bot.js
@@ -120,12 +120,7 @@ async function botScript() {
 			retweetBestTweet(bestTweetId);
 		}
 	} else {
-		console.log(
-			'No matching results to display at this time, running bot script again until matching results are found.'
-		);
-		for (i = 0; i < 3; i++) {
-			botScript();
-		}
+		console.log('No matching results to display at this time.');
 	}
 }
 

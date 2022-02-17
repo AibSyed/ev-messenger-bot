4036db33194409cef8aa57be46a9cf09ed9dc9f4
AibSyed <asyed1447@gmail.com>
2022-02-16 23:17:21 -0600

    add more comments to array, increase start time and botScript interval time

--- bot.js
+++ bot.js
@@ -23,17 +23,17 @@ const TENTH_USER_LIST = process.env['TENTH_USER_LIST'];
 
 //an authenticated client for this app
 const app = new TwitterLite({
-    version: '2',
-    extension: false,
-    bearer_token: TWITTER_BEARER_TOKEN,
+	version: '2',
+	extension: false,
+	bearer_token: TWITTER_BEARER_TOKEN,
 });
 
 //an authenticated client for the bot user
 const user = new TwitterLite({
-    access_token_key: TWITTER_ACCESS_TOKEN,
-    access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
-    consumer_key: TWITTER_API_KEY,
-    consumer_secret: TWITTER_API_SECRET,
+	access_token_key: TWITTER_ACCESS_TOKEN,
+	access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
+	consumer_key: TWITTER_API_KEY,
+	consumer_secret: TWITTER_API_SECRET,
 });
 
 let userGroup1 = [FIRST_USER_LIST, SIXTH_USER_LIST];
@@ -48,105 +48,105 @@ let userGroup5 = [THIRD_USER_LIST, TENTH_USER_LIST];
 
 //pick random userlist based on mode value and return result
 function pickRandomUserList() {
-    console.log('Running function to grab random user list');
-    let userList;
-    let mode = Math.random();
-    let randomUserList1 =
-        userGroup1[Math.floor(userGroup1.length * Math.random())];
-    let randomUserList2 =
-        userGroup2[Math.floor(userGroup2.length * Math.random())];
-    let randomUserList3 =
-        userGroup3[Math.floor(userGroup3.length * Math.random())];
-    let randomUserList4 =
-        userGroup4[Math.floor(userGroup4.length * Math.random())];
-    let randomUserList5 =
-        userGroup5[Math.floor(userGroup5.length * Math.random())];
+	console.log('Running function to grab random user list');
+	let userList;
+	let mode = Math.random();
+	let randomUserList1 =
+		userGroup1[Math.floor(userGroup1.length * Math.random())];
+	let randomUserList2 =
+		userGroup2[Math.floor(userGroup2.length * Math.random())];
+	let randomUserList3 =
+		userGroup3[Math.floor(userGroup3.length * Math.random())];
+	let randomUserList4 =
+		userGroup4[Math.floor(userGroup4.length * Math.random())];
+	let randomUserList5 =
+		userGroup5[Math.floor(userGroup5.length * Math.random())];
 
-    if (mode > 0 && mode <= 0.2) {
-        userList = randomUserList1;
-        console.log('Picking random user list from group 1');
-    } else if (mode > 0.2 && mode <= 0.4) {
-        userList = randomUserList2;
-        console.log('Picking random user list from group 2');
-    } else if (mode > 0.4 && mode <= 0.6) {
-        userList = randomUserList3;
-        console.log('Picking random user list from group 3');
-    } else if (mode > 0.6 && mode <= 0.8) {
-        userList = randomUserList4;
-        console.log('Picking random user list from group 4');
-    } else {
-        userList = randomUserList5;
-        console.log('Picking random user list from group 5');
-    }
-    console.log(userList + ' is the winner.');
-    return userList;
+	if (mode > 0 && mode <= 0.2) {
+		userList = randomUserList1;
+		console.log('Picking random user list from group 1');
+	} else if (mode > 0.2 && mode <= 0.4) {
+		userList = randomUserList2;
+		console.log('Picking random user list from group 2');
+	} else if (mode > 0.4 && mode <= 0.6) {
+		userList = randomUserList3;
+		console.log('Picking random user list from group 3');
+	} else if (mode > 0.6 && mode <= 0.8) {
+		userList = randomUserList4;
+		console.log('Picking random user list from group 4');
+	} else {
+		userList = randomUserList5;
+		console.log('Picking random user list from group 5');
+	}
+	console.log(userList + ' is the winner.');
+	return userList;
 }
 
 async function botScript() {
-    console.log('Bot script has started');
+	console.log('Bot script has started');
 
-    //IIFE (Immediately Invoked Function Expression to get chosen user list)
-    let userList = pickRandomUserList();
+	//IIFE (Immediately Invoked Function Expression to get chosen user list)
+	let userList = pickRandomUserList();
 
-    const fullQuery =
-        '(' + helper.getFromClauses(userList) + ') -is:reply -is:retweet';
+	const fullQuery =
+		'(' + helper.getFromClauses(userList) + ') -is:reply -is:retweet';
 
-    let pastTenMinutes = new Date();
-    let reducedMinutes = 5;
-    pastTenMinutes.setDate(pastTenMinutes.getDate());
-    pastTenMinutes.setMinutes(pastTenMinutes.getMinutes() - reducedMinutes);
+	let pastTenMinutes = new Date();
+	let reducedMinutes = 20;
+	pastTenMinutes.setDate(pastTenMinutes.getDate());
+	pastTenMinutes.setMinutes(pastTenMinutes.getMinutes() - reducedMinutes);
 
-    let params = {
-        start_time: pastTenMinutes.toISOString(),
-        max_results: 15,
-        'tweet.fields': 'public_metrics',
-        expansions: 'author_id',
-        'user.fields': 'id,username',
-        query: fullQuery,
-    };
+	let params = {
+		start_time: pastTenMinutes.toISOString(),
+		max_results: 15,
+		'tweet.fields': 'public_metrics',
+		expansions: 'author_id',
+		'user.fields': 'id,username',
+		query: fullQuery,
+	};
 
-    const { meta, data, includes } = await app.get(
-        'tweets/search/recent',
-        params
-    );
-    if (meta.result_count > 0) {
-        const { bestTweetId, bestTweetUser } = helper.getBestTweet(data);
-        console.log(
-            'Found results that match criteria, determining whether to quote tweet, or retweet'
-        );
-        if (Math.random() > 0.1) {
-            quoteTweetBestTweet(bestTweetId, bestTweetUser, includes);
-        } else {
-            retweetBestTweet(bestTweetId);
-        }
-    } else {
-        console.log('No matching results to display at this time.');
-    }
+	const { meta, data, includes } = await app.get(
+		'tweets/search/recent',
+		params
+	);
+	if (meta.result_count > 0) {
+		const { bestTweetId, bestTweetUser } = helper.getBestTweet(data);
+		console.log(
+			'Found results that match criteria, determining whether to quote tweet, or retweet'
+		);
+		if (Math.random() > 0.1) {
+			quoteTweetBestTweet(bestTweetId, bestTweetUser, includes);
+		} else {
+			retweetBestTweet(bestTweetId);
+		}
+	} else {
+		console.log('No matching results to display at this time.');
+	}
 }
 
 async function quoteTweetBestTweet(bestTweetId, bestTweetUser, includes) {
-    console.log('Running Quote Best Tweet Function');
-    console.log('this is the best tweet id: ' + bestTweetId);
-    const username = helper.getUsernameFromId(includes.users, bestTweetUser);
-    if (username != undefined) {
-        const status = helper.getStatus(bestTweetId, username);
-        console.log(status);
-        try {
-            const { data } = await user.post('statuses/update', { status: status });
-        } catch (err) {
-            console.log(err);
-        }
-    }
+	console.log('Running Quote Best Tweet Function');
+	console.log('this is the best tweet id: ' + bestTweetId);
+	const username = helper.getUsernameFromId(includes.users, bestTweetUser);
+	if (username != undefined) {
+		const status = helper.getStatus(bestTweetId, username);
+		console.log(status);
+		try {
+			const { data } = await user.post('statuses/update', { status: status });
+		} catch (err) {
+			console.log(err);
+		}
+	}
 }
 
 async function retweetBestTweet(id) {
-    console.log('Running Best Retweet Function');
-    try {
-        const { data } = await user.post('statuses/retweet/' + id);
-    } catch (err) {
-        console.log(err);
-    }
+	console.log('Running Best Retweet Function');
+	try {
+		const { data } = await user.post('statuses/retweet/' + id);
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 //run every 5 minutes
-setInterval(botScript, 300000);
+setInterval(botScript, 1200000);

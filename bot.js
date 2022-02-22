e06c4e22b6faa8a8102ef9fd59a5a167240d6b5f
AibSyed <asyed1447@gmail.com>
2022-02-22 00:08:34 -0600

    create utls folder, relocate comment and userlist functions to picker.js, add comments for context

--- bot.js
+++ bot.js
@@ -1,151 +1,100 @@
 console.log('EV-Messenger-Bot is running...');
 
+//import dependencies
 require('dotenv').config();
-const helper = require('./helper.js');
 const TwitterLite = require('twitter-lite');
 
-//declare auth vars and user lists from env
+//import utilities
+const helper = require('./utils/helper.js');
+const picker = require('./utils/picker.js');
+
+//declare twitter auth vars from env
 const TWITTER_ACCESS_TOKEN_SECRET = process.env['TWITTER_ACCESS_TOKEN_SECRET'];
 const TWITTER_ACCESS_TOKEN = process.env['TWITTER_ACCESS_TOKEN'];
 const TWITTER_API_SECRET = process.env['TWITTER_API_SECRET'];
 const TWITTER_API_KEY = process.env['TWITTER_API_KEY'];
 const TWITTER_BEARER_TOKEN = process.env['TWITTER_BEARER_TOKEN'];
-const FIRST_USER_LIST = process.env['FIRST_USER_LIST'];
-const SECOND_USER_LIST = process.env['SECOND_USER_LIST'];
-const THIRD_USER_LIST = process.env['THIRD_USER_LIST'];
-const FOURTH_USER_LIST = process.env['FOURTH_USER_LIST'];
-const FIFTH_USER_LIST = process.env['FIFTH_USER_LIST'];
-const SIXTH_USER_LIST = process.env['SIXTH_USER_LIST'];
-const SEVENTH_USER_LIST = process.env['SEVENTH_USER_LIST'];
-const EIGHTH_USER_LIST = process.env['EIGHTH_USER_LIST'];
-const NINTH_USER_LIST = process.env['NINTH_USER_LIST'];
-const TENTH_USER_LIST = process.env['TENTH_USER_LIST'];
 
-//an authenticated client for this app
+//declare authenticated client for app
 const app = new TwitterLite({
-    version: '2',
-    extension: false,
-    bearer_token: TWITTER_BEARER_TOKEN,
+	version: '2',
+	extension: false,
+	bearer_token: TWITTER_BEARER_TOKEN,
 });
 
-//an authenticated client for the bot user
+//declare authenticated client for bot user
 const user = new TwitterLite({
-    access_token_key: TWITTER_ACCESS_TOKEN,
-    access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
-    consumer_key: TWITTER_API_KEY,
-    consumer_secret: TWITTER_API_SECRET,
+	access_token_key: TWITTER_ACCESS_TOKEN,
+	access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
+	consumer_key: TWITTER_API_KEY,
+	consumer_secret: TWITTER_API_SECRET,
 });
 
-let userGroup1 = [FIRST_USER_LIST, SIXTH_USER_LIST];
-
-let userGroup2 = [SECOND_USER_LIST, SEVENTH_USER_LIST];
-
-let userGroup3 = [NINTH_USER_LIST, FIFTH_USER_LIST];
-
-let userGroup4 = [EIGHTH_USER_LIST, FOURTH_USER_LIST];
-
-let userGroup5 = [THIRD_USER_LIST, TENTH_USER_LIST];
-
-//pick random userlist based on mode value and return result
-function pickRandomUserList() {
-    console.log('Running function to grab random user list');
-    let userList;
-    let mode = Math.random();
-    let randomUserList1 =
-        userGroup1[Math.floor(userGroup1.length * Math.random())];
-    let randomUserList2 =
-        userGroup2[Math.floor(userGroup2.length * Math.random())];
-    let randomUserList3 =
-        userGroup3[Math.floor(userGroup3.length * Math.random())];
-    let randomUserList4 =
-        userGroup4[Math.floor(userGroup4.length * Math.random())];
-    let randomUserList5 =
-        userGroup5[Math.floor(userGroup5.length * Math.random())];
-
-    if (mode > 0 && mode <= 0.2) {
-        userList = randomUserList1;
-        console.log('Picking random user list from group 1');
-    } else if (mode > 0.2 && mode <= 0.4) {
-        userList = randomUserList2;
-        console.log('Picking random user list from group 2');
-    } else if (mode > 0.4 && mode <= 0.6) {
-        userList = randomUserList3;
-        console.log('Picking random user list from group 3');
-    } else if (mode > 0.6 && mode <= 0.8) {
-        userList = randomUserList4;
-        console.log('Picking random user list from group 4');
-    } else {
-        userList = randomUserList5;
-        console.log('Picking random user list from group 5');
-    }
-    console.log(userList + ' is the winner.');
-    return userList;
-}
-
+//main script
 async function botScript() {
-    console.log('Bot script has started');
+	console.log('Bot script has started');
+	//IIFE (Immediately Invoked Function Expression to get chosen user list)
+	let userList = picker.pickRandomUserList();
 
-    //IIFE (Immediately Invoked Function Expression to get chosen user list)
-    let userList = pickRandomUserList();
+	const fullQuery =
+		'(' + helper.getFromClauses(userList) + ') -is:reply -is:retweet';
 
-    const fullQuery =
-        '(' + helper.getFromClauses(userList) + ') -is:reply -is:retweet';
+	let pastTenMinutes = new Date();
+	let reducedMinutes = 10;
+	pastTenMinutes.setDate(pastTenMinutes.getDate());
+	pastTenMinutes.setMinutes(pastTenMinutes.getMinutes() - reducedMinutes);
 
-    let pastTenMinutes = new Date();
-    let reducedMinutes = 10;
-    pastTenMinutes.setDate(pastTenMinutes.getDate());
-    pastTenMinutes.setMinutes(pastTenMinutes.getMinutes() - reducedMinutes);
+	let params = {
+		start_time: pastTenMinutes.toISOString(),
+		max_results: 15,
+		'tweet.fields': 'public_metrics',
+		expansions: 'author_id',
+		'user.fields': 'id,username',
+		query: fullQuery,
+	};
 
-    let params = {
-        start_time: pastTenMinutes.toISOString(),
-        max_results: 15,
-        'tweet.fields': 'public_metrics',
-        expansions: 'author_id',
-        'user.fields': 'id,username',
-        query: fullQuery,
-    };
-
-    const { meta, data, includes } = await app.get(
-        'tweets/search/recent',
-        params
-    );
-    if (meta.result_count > 0) {
-        const { bestTweetId, bestTweetUser } = helper.getBestTweet(data);
-        console.log(
-            'Found results that match criteria, determining whether to quote tweet, or retweet'
-        );
-        if (Math.random() > 0.1) {
-            quoteTweetBestTweet(bestTweetId, bestTweetUser, includes);
-        } else {
-            retweetBestTweet(bestTweetId);
-        }
-    } else {
-        console.log('No matching results to display at this time.');
-    }
+	const { meta, data, includes } = await app.get(
+		'tweets/search/recent',
+		params
+	);
+	if (meta.result_count > 0) {
+		const { bestTweetId, bestTweetUser } = helper.getBestTweet(data);
+		console.log(
+			'Found results that match criteria, determining whether to quote tweet, or retweet'
+		);
+		if (Math.random() > 0.1) {
+			quoteTweetBestTweet(bestTweetId, bestTweetUser, includes);
+		} else {
+			retweetBestTweet(bestTweetId);
+		}
+	} else {
+		console.log('No matching results to display at this time.');
+	}
 }
 
 async function quoteTweetBestTweet(bestTweetId, bestTweetUser, includes) {
-    console.log('Running Quote Best Tweet Function');
-    console.log('this is the best tweet id: ' + bestTweetId);
-    const username = helper.getUsernameFromId(includes.users, bestTweetUser);
-    if (username != undefined) {
-        const status = helper.getStatus(bestTweetId, username);
-        console.log(status);
-        try {
-            const { data } = await user.post('statuses/update', { status: status });
-        } catch (err) {
-            console.log(err);
-        }
-    }
+	console.log('Running Quote Best Tweet Function');
+	console.log('this is the best tweet id: ' + bestTweetId);
+	const username = helper.getUsernameFromId(includes.users, bestTweetUser);
+	if (username != undefined) {
+		const status = helper.getStatus(bestTweetId, username);
+		console.log(status);
+		try {
+			const { data } = await user.post('statuses/update', { status: status });
+		} catch (err) {
+			console.log(err);
+		}
+	}
 }
 
 async function retweetBestTweet(id) {
-    console.log('Running Best Retweet Function');
-    try {
-        const { data } = await user.post('statuses/retweet/' + id);
-    } catch (err) {
-        console.log(err);
-    }
+	console.log('Running Best Retweet Function');
+	console.log('this is the  retweet id: ' + id);
+	try {
+		const { data } = await user.post('statuses/retweet/' + id);
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 //run every 5 minutes
---
+++ utils/picker.js
@@ -0,0 +1,89 @@
+//dependencies
+require('dotenv').config();
+
+//declare user lists from .env
+const FIRST_USER_LIST = process.env['FIRST_USER_LIST'];
+const SECOND_USER_LIST = process.env['SECOND_USER_LIST'];
+const THIRD_USER_LIST = process.env['THIRD_USER_LIST'];
+const FOURTH_USER_LIST = process.env['FOURTH_USER_LIST'];
+const FIFTH_USER_LIST = process.env['FIFTH_USER_LIST'];
+const SIXTH_USER_LIST = process.env['SIXTH_USER_LIST'];
+const SEVENTH_USER_LIST = process.env['SEVENTH_USER_LIST'];
+const EIGHTH_USER_LIST = process.env['EIGHTH_USER_LIST'];
+const NINTH_USER_LIST = process.env['NINTH_USER_LIST'];
+const TENTH_USER_LIST = process.env['TENTH_USER_LIST'];
+
+//declare user group arrays
+let userGroup1 = [FIRST_USER_LIST, SIXTH_USER_LIST];
+let userGroup2 = [SECOND_USER_LIST, SEVENTH_USER_LIST];
+let userGroup3 = [NINTH_USER_LIST, FIFTH_USER_LIST];
+let userGroup4 = [EIGHTH_USER_LIST, FOURTH_USER_LIST];
+let userGroup5 = [THIRD_USER_LIST, TENTH_USER_LIST];
+
+//randomize user list selection from user group arrays
+let randomUserList1 = userGroup1[Math.floor(userGroup1.length * Math.random())];
+let randomUserList2 = userGroup2[Math.floor(userGroup2.length * Math.random())];
+let randomUserList3 = userGroup3[Math.floor(userGroup3.length * Math.random())];
+let randomUserList4 = userGroup4[Math.floor(userGroup4.length * Math.random())];
+let randomUserList5 = userGroup5[Math.floor(userGroup5.length * Math.random())];
+
+//pick random user list based on mode value and return result
+const pickRandomUserList = function () {
+	console.log('Running function to grab random user list');
+	let mode = Math.random();
+	let userList;
+	if (mode > 0 && mode <= 0.2) {
+		userList = randomUserList1;
+		console.log('Picking random user list from group 1');
+	} else if (mode > 0.2 && mode <= 0.4) {
+		userList = randomUserList2;
+		console.log('Picking random user list from group 2');
+	} else if (mode > 0.4 && mode <= 0.6) {
+		userList = randomUserList3;
+		console.log('Picking random user list from group 3');
+	} else if (mode > 0.6 && mode <= 0.8) {
+		userList = randomUserList4;
+		console.log('Picking random user list from group 4');
+	} else {
+		userList = randomUserList5;
+		console.log('Picking random user list from group 5');
+	}
+	return userList;
+};
+
+// pick random comment
+const pickRandomComment = function (user) {
+	// set array of comments
+	let comments = [
+		user + ' shared the following post...',
+		'The latest update from ' + user + ':',
+		'Check out ' + user + "'s latest post! ⚡",
+		"Here's the latest from " + user + ':',
+		user,
+		'A new post from ' + user + ':',
+		'What do you think about this post from ' + user + '?',
+		"Here's what " + user + ' has been up to:',
+		'What are your thoughts on this post from ' + user + '?',
+		'Another EV update from ' + user + '!',
+		"If you haven't aleady, give " + user + ' a follow for more great posts!',
+		'Another great post from ' + user,
+		'Boom, another EV post 💥',
+		'I am the most electrifying man in Tweet entertainment!⚡⚡⚡- Make sure you check out this post from ' +
+			user +
+			'.',
+		'Can I interest you in the following tweet from ' + user + '?',
+		"If you're not getting at least some of your EV news from me, you're simply not doing it right... Here's another post, courtesy of " +
+			user +
+			'.',
+		"Here's another post for my followers. - For those that don't follow me, what are you waiting for?",
+		"I've got the plug ⚡. Check out this post from " + user,
+	];
+	//select random comments based on length of comments array
+	let randomComment = comments[Math.floor(comments.length * Math.random())];
+
+	return randomComment;
+};
+
+//exports
+exports.pickRandomUserList = pickRandomUserList;
+exports.pickRandomComment = pickRandomComment;

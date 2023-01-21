7a60d7040c11f521bea51b175bccb2f0c0df8e27
AibSyed <asyed1447@gmail.com>
2023-01-21 16:45:24 -0600

    Reconfigured logic for the getBestTweet function to reduce the amount of points needed for a tweet to be considered for a post. Also included descriptive comments to explain code within the helper.js file and added additional logs that will fire during code execution.

--- utils/helper.js
+++ utils/helper.js
@@ -1,38 +1,55 @@
-//import utilities
+// import utilities from the file picker.js
 const picker = require('./picker.js');
 
+// function that calculates the best tweet from an array of tweets
 const getBestTweet = function (data) {
+	console.log('Calculating best tweet...');
+	// initialize variables to track the best tweet
 	let bestTweetId = 0,
 		bestTweetPoints = 0,
 		bestTweetUser = '';
+	// loop through the tweets and calculate the points for each one
 	data.forEach((tweet) => {
 		const metrics = tweet.public_metrics;
 		let currentTweetPoints =
-			metrics.like_count +
-			metrics.retweet_count * 5 +
-			metrics.quote_count * 2 +
-			metrics.reply_count * 2;
+			metrics.like_count * 0.75 +
+			metrics.retweet_count * 0.75 +
+			metrics.quote_count * 0.75 +
+			metrics.reply_count * 0.75;
+		// check if this tweet has more points than the current best tweet
 		if (currentTweetPoints > bestTweetPoints) {
+			// if so, update the best tweet variables
 			bestTweetPoints = currentTweetPoints;
 			bestTweetId = tweet.id;
 			bestTweetUser = tweet.author_id;
 		}
 	});
-
+	console.log('Best tweet:', bestTweetId, bestTweetUser);
+	// return an object with the best tweet's ID and user
 	return { bestTweetId: bestTweetId, bestTweetUser: bestTweetUser };
 };
 
+// function that creates a "from" clause for a Twitter query
 const getFromClauses = function (userList) {
+	console.log('Creating "from" clause...');
 	let fromClause = '';
+	// split the user list into an array
 	const users = userList.split(',');
+	// loop through the users and add them to the from clause
 	for (const user in users) {
 		fromClause += 'OR ' + 'from:' + users[user] + ' ';
 	}
+	// remove the first "OR" from the clause
 	fromClause = fromClause.substring(3);
+	console.log('"From" clause:', fromClause);
+	// return the final from clause
 	return fromClause;
 };
 
+// function that creates a tweet status string
 const getStatus = function (tweetId, user) {
+	console.log('Creating tweet status...');
+	// return a string that includes a random comment and a link to the tweet
 	return (
 		picker.pickRandomComment(user) +
 		'\nhttps://twitter.com/' +
@@ -42,16 +59,20 @@ const getStatus = function (tweetId, user) {
 	);
 };
 
+// function that gets a username from a user ID
 const getUsernameFromId = function (users, id) {
+	console.log('Getting username from user ID...');
+	// loop through the users and check the id
 	for (const user in users) {
 		obj = users[user];
 		if (obj.id === id) {
+			// return the username if the id matches
 			return obj.username;
 		}
 	}
 };
 
-//exports
+// export the functions for use in other modules
 exports.getFromClauses = getFromClauses;
 exports.getBestTweet = getBestTweet;
 exports.getStatus = getStatus;

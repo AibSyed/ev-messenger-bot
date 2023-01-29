6a4cf20b5839ed34c45e803aee4d5ff607797061
AibSyed <53330015+AibSyed@users.noreply.github.com>
2023-01-29 15:56:01 -0600

    Add try/catch block to api call for recent tweets

--- bot.js
+++ bot.js
@@ -18,134 +18,138 @@ const TWITTER_BEARER_TOKEN = process.env['TWITTER_BEARER_TOKEN'];
 
 //Declare authenticated client for app
 const app = new TwitterLite({
-	version: '2',
-	extension: false,
-	bearer_token: TWITTER_BEARER_TOKEN,
+    version: '2',
+    extension: false,
+    bearer_token: TWITTER_BEARER_TOKEN,
 });
 
 //Declare authenticated client for bot user
 const user = new TwitterLite({
-	access_token_key: TWITTER_ACCESS_TOKEN,
-	access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
-	consumer_key: TWITTER_API_KEY,
-	consumer_secret: TWITTER_API_SECRET,
+    access_token_key: TWITTER_ACCESS_TOKEN,
+    access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
+    consumer_key: TWITTER_API_KEY,
+    consumer_secret: TWITTER_API_SECRET,
 });
 
 //Main script
 async function botScript() {
-	console.log('Bot script has started.');
-	//Get a list of chosen users by invoking the pickRandomUserList function from picker.js
-	let userList = picker.pickRandomUserList();
-	//Build the query string using the chosen user list and excluding replies and retweets
-	const fullQuery =
-		'(' + helper.getFromClauses(userList) + ') -is:reply -is:retweet';
+    console.log('Bot script has started.');
+    //Get a list of chosen users by invoking the pickRandomUserList function from picker.js
+    let userList = picker.pickRandomUserList();
+    //Build the query string using the chosen user list and excluding replies and retweets
+    const fullQuery =
+        '(' + helper.getFromClauses(userList) + ') -is:reply -is:retweet';
 
-	//Create a new Date object
-	let currentDate = new Date();
+    //Create a new Date object
+    let currentDate = new Date();
 
-	//Reduce the minutes by 6
-	let reducedMinutes = 60;
+    //Reduce the minutes by 6
+    let reducedMinutes = 60;
 
-	//Subtract the reduced minutes from the current time
-	currentDate.setMinutes(currentDate.getMinutes() - reducedMinutes);
+    //Subtract the reduced minutes from the current time
+    currentDate.setMinutes(currentDate.getMinutes() - reducedMinutes);
 
-	//Convert the Date object to ISO String format
-	let searchStartTime = currentDate.toISOString();
+    //Convert the Date object to ISO String format
+    let searchStartTime = currentDate.toISOString();
 
-	//Set the parameters for the Twitter API call
-	let params = {
-		//Pass the ISO String representation of the date object
-		start_time: searchStartTime,
-		//Set the maximum number of results to be returned
-		max_results: 15,
-		//Select the specific tweet fields to be returned
-		'tweet.fields': 'public_metrics',
-		//Expand the author_id field to get additional information
-		expansions: 'author_id',
-		//Select the specific user fields to be returned
-		'user.fields': 'id,username',
-		//Pass the full query string
-		query: fullQuery,
-	};
+    //Set the parameters for the Twitter API call
+    let params = {
+        //Pass the ISO String representation of the date object
+        start_time: searchStartTime,
+        //Set the maximum number of results to be returned
+        max_results: 15,
+        //Select the specific tweet fields to be returned
+        'tweet.fields': 'public_metrics',
+        //Expand the author_id field to get additional information
+        expansions: 'author_id',
+        //Select the specific user fields to be returned
+        'user.fields': 'id,username',
+        //Pass the full query string
+        query: fullQuery,
+    };
 
-	//Make the API call and destructure the response
-	console.log('Making API call with parameters: ', params);
-	const { meta, data, includes } = await app.get(
-		'tweets/search/recent',
-		params
-	);
-	console.log('API call result : ', meta.result_count);
-	//Check if there are any matching results
-	if (meta.result_count > 0) {
-		// Get the best tweet from the matching results using helper function
-		const { bestTweetId, bestTweetUser, bestTweetPoints } =
-			helper.getBestTweet(data);
+    //Make the API call and destructure the response
+    console.log('Making API call with parameters: ', params);
+    try {
+        const { meta, data, includes } = await app.get('tweets/search/recent', params);
+        console.log('API call result : ', meta.result_count);
+        //Check if there are any matching results
+        //...
+    } catch (err) {
+        console.log(err);
+    }
+    console.log('API call result : ', meta.result_count);
+    //Check if there are any matching results
+    if (meta.result_count > 0) {
+        // Get the best tweet from the matching results using helper function
+        const { bestTweetId, bestTweetUser, bestTweetPoints } =
+            helper.getBestTweet(data);
 
-		//Check if the tweet has already been posted
-		if (tweetIdStorage.hasTweetId(bestTweetId)) {
-			console.log(
-				`Tweet with ID ${bestTweetId} has already been posted. Skipping...`
-			);
-			return;
-		}
+        //Check if the tweet has already been posted
+        if (tweetIdStorage.hasTweetId(bestTweetId)) {
+            console.log(
+                `Tweet with ID ${bestTweetId} has already been posted. Skipping...`
+            );
+            return;
+        }
 
-		// Log the details of the best tweet
-		console.log(
-			`Found ${meta.result_count} results that match criteria, determining best tweet with Id: ${bestTweetId}, Author: ${bestTweetUser} and Score: ${bestTweetPoints}`
-		);
+        // Log the details of the best tweet
+        console.log(
+            `Found ${meta.result_count} results that match criteria, determining best tweet with Id: ${bestTweetId}, Author: ${bestTweetUser} and Score: ${bestTweetPoints}`
+        );
 
-		// Randomly decide to either quote tweet or retweet the best tweet
-		if (Math.random() > 0.1) {
-			console.log(
-				`Tweet with ID ${bestTweetId} has been selected for a quote tweet.`
-			);
-			quoteTweetBestTweet(bestTweetId, bestTweetUser, includes);
-		} else {
-			console.log(
-				`Tweet with ID ${bestTweetId} has been selected for a retweet.`
-			);
-			retweetBestTweet(bestTweetId);
-		}
+        // Randomly decide to either quote tweet or retweet the best tweet
+        if (Math.random() > 0.1) {
+            console.log(
+                `Tweet with ID ${bestTweetId} has been selected for a quote tweet.`
+            );
+            quoteTweetBestTweet(bestTweetId, bestTweetUser, includes);
+        } else {
+            console.log(
+                `Tweet with ID ${bestTweetId} has been selected for a retweet.`
+            );
+            retweetBestTweet(bestTweetId);
+        }
 
-		// Add the tweet ID to the list of posted tweets
-		tweetIdStorage.addTweetId(bestTweetId);
-	} else {
-		console.log('No results to display at this time.');
-	}
+        // Add the tweet ID to the list of posted tweets
+        tweetIdStorage.addTweetId(bestTweetId);
+    } else {
+        console.log('No results to display at this time.');
+    }
 }
 
 //Function for quoting the best tweet
 async function quoteTweetBestTweet(bestTweetId, bestTweetUser, includes) {
-	console.log('Running Quote Best Tweet function.');
-	console.log(`Best tweet id: ${bestTweetId}`);
+    console.log('Running Quote Best Tweet function.');
+    console.log(`Best tweet id: ${bestTweetId}`);
 
-	//Get the username of the best tweet user
-	const username = helper.getUsernameFromId(includes.users, bestTweetUser);
+    //Get the username of the best tweet user
+    const username = helper.getUsernameFromId(includes.users, bestTweetUser);
 
-	//If the username is defined, create the status for the quote tweet
-	if (username != undefined) {
-		const status = helper.getStatus(bestTweetId, username);
-		console.log(`Tweeting the following status for ${username}: ${status}`);
-		try {
-			const { data } = await user.post('statuses/update', { status: status });
-			console.log('Quote tweet successful');
-		} catch (err) {
-			console.log(`Error occurred while trying to quote tweet: ${err.message}`);
-		}
-	}
+    //If the username is defined, create the status for the quote tweet
+    if (username != undefined) {
+        const status = helper.getStatus(bestTweetId, username);
+        console.log(`Tweeting the following status for ${username}: ${status}`);
+        try {
+            const { data } = await user.post('statuses/update', { status: status });
+            console.log('Quote tweet successful');
+        } catch (err) {
+            console.log(`Error occurred while trying to quote tweet: ${err.message}`);
+        }
+    }
 }
 
 //Function for retweeting the best tweet
 async function retweetBestTweet(id) {
-	console.log('Running Retweet Best Tweet function.');
-	console.log(`Best retweet id: ${id}`);
-	try {
-		const { data } = await user.post(`statuses/retweet/${id}`);
-		console.log(`Retweeted tweet with ID: ${id}`);
-	} catch (err) {
-		console.log(`Error occurred while trying to retweet tweet with ID: ${id}`);
-		console.log(`Error message: ${err.message}`);
-	}
+    console.log('Running Retweet Best Tweet function.');
+    console.log(`Best retweet id: ${id}`);
+    try {
+        const { data } = await user.post(`statuses/retweet/${id}`);
+        console.log(`Retweeted tweet with ID: ${id}`);
+    } catch (err) {
+        console.log(`Error occurred while trying to retweet tweet with ID: ${id}`);
+        console.log(`Error message: ${err.message}`);
+    }
 }
 
 //Run every 2.5 minutes

7bb1d721b17727c027dd00bdf2f991d35c459087
AibSyed <asyed1447@gmail.com>
2023-02-01 01:41:22 -0600

    revert pickRandomComment function and increase limit

--- utils/picker.js
+++ utils/picker.js
@@ -1,9 +1,6 @@
 //dependencies
 require('dotenv').config();
 
-// import utilities from the file comments.js
-const tweetComments = require('./comments.js');
-
 //declare user lists from .env
 const userList01 = process.env['USER_LIST_01'];
 const userList02 = process.env['USER_LIST_02'];
@@ -52,50 +49,126 @@ const pickRandomUserList = function () {
 	return userList;
 };
 
-// Array to store selected comments from pickRandomComment function
+// array to store selected comments from pickRandomComment function
 let selectedComments = [];
 
-// Array of comments to be used
-const comments = tweetComments;
+// limit of comments
+const commentLimit = 10;
 
-// Function to pick a random comment from the list of comments
+// function to pick a random comment from the list of comments
 const pickRandomComment = function (user) {
 	console.log('Running function to pick random comment for status.');
-	// Check if all comments have been used
-	if (comments.length === 0) {
-		console.log('All comments have been used. Resetting...');
-
-		// Reset the comments array by pushing all values from the selectedComments array back into it
-		comments.push(...selectedComments);
-
-		// Empty the selectedComments array
-		selectedComments = [];
+	// set array of comments
+	let comments = [
+		user + ' shared the following post...',
+		'The latest update from ' + user + ':',
+		'Check out ' + user + "'s latest post! ⚡",
+		"Here's the latest from " + user + ':',
+		user,
+		'A new post from ' + user + ':',
+		'What do you think about this post from ' + user + '?',
+		"Here's what " + user + ' has been up to:',
+		'What are your thoughts on this post from ' + user + '?',
+		'Another EV update from ' + user + '!',
+		"If you haven't already, give " + user + ' a follow for more great posts!',
+		'Another great post from ' + user,
+		'Boom, another EV post from ' + user + ' 💥',
+		"If you're not getting at least some of your EV news from me, you're simply not doing it right... Here's another post, courtesy of " +
+			user +
+			'.',
+		"Here's another post for my followers. - For those that don't follow me, what are you waiting for?",
+		"I've got the plug 🔌. Check out this post from " + user,
+		'Just stumbled upon a great post from ' + user + ', check it out!',
+		'Looking for some inspiration on the latest EV developments? Give ' +
+			user +
+			'a follow! Here is their latest post:',
+		'I always keep an eye out for interesting posts on the EV market and this one from ' +
+			user +
+			' caught my attention:',
+		"If you're interested in the latest EV developments, this post from " +
+			user +
+			' is worth a look.',
+		'I came across this post from ' +
+			user +
+			" and I think it's worth sharing. Check it out!",
+		'This post from ' + user + ' is 👌',
+		"As an EV enthusiast, I couldn't resist sharing this post from " +
+			user +
+			'.',
+		'This post from ' +
+			user +
+			' caught my attention. Take a look and give them a like/follow!',
+		'I thought this post from ' + user + ' was worth sharing, enjoy!',
+		"See what's new from " + user + ':',
+		user + ' a great post to share, give it a like and give them a follow!',
+		'What do you think about ' + user + "'s latest post?",
+		"What's your opinion on " + user + "'s latest post?",
+		'Stay updated with another post from ' + user + '!',
+		"Don't miss out, give " + user + ' a follow for more amazing posts!',
+		'More great content from ' + user,
+		user + ' just posted another electrifying update!⚡⚡⚡',
+		"If you're not following " + user + ", you're missing out!",
+		'Check out this post from ' + user + ' and stay updated!',
+		'Informative post from ' + user,
+		'I just found this amazing post from ' + user + '. Check it out!',
+		user + ' with a great post. Like and follow!',
+		"I'm loving the insights in this post from " + user,
+		'Find inspiration with this post from ' + user,
+		"I'm always on the lookout for interesting EV posts and this one from " +
+			user +
+			' is a must-see.',
+		'I just stumbled upon this post from ' +
+			user +
+			' and thought it was worth sharing.',
+		"Don't miss this post from " + user + '!',
+		'Take a look at this post from ' + user + ' on the latest EV developments.',
+		"Don't miss out on the latest thunderous insights from " + user + ' ⚡⚡⚡',
+		"You don't want to be left in the dark on the latest EV developments. Follow " +
+			user +
+			' for some thunderous updates ⚡⚡⚡',
+		user +
+			' is the ultimate EV oracle. Their latest post will enlighten you 🔮',
+		'Get ready to be struck by lightning with the latest post from ' +
+			user +
+			' ⚡',
+		"It's time for some electrifying news from " + user + '. Follow them now!',
+		'Get ready to feel the power of the EV gods with ' +
+			user +
+			"'s latest post 💪⚡",
+		'The EV industry is never dull with ' +
+			user +
+			' on the scene. Check out their latest post!',
+		'As the EV god, I bestow upon you the latest post from ' +
+			user +
+			'. Read and be enlightened, give them a follow! 💡',
+		'The EV industry is about to get a shock with this latest post from ' +
+			user +
+			' ⚡',
+		"As an EV god, I'm always on the lookout for the best EV insights. Check out this latest post from " +
+			user +
+			'!',
+		"I've got the power, and so does " +
+			user +
+			'. Check out their latest post on the latest EV developments 💥',
+	];
+	//select random comments based on length of comments array
+	let randomComment = comments[Math.floor(comments.length * Math.random())];
+	// check if selectedComment length is equal to the limit
+	if (selectedComments.length === commentLimit) {
+		console.log(
+			`Limit of ${commentLimit} comments reached, resetting selected comments`
+		);
+		selectedComments = []; // reset the array
 	}
-
-	// Select a random comment from the comments array
-	let randomIndex = Math.floor(Math.random() * comments.length);
-	let selectedComment = comments[randomIndex];
-
-	// substitute the user name into the comment string
-	selectedComment = selectedComment.replace('user', user);
-
-	// Remove the selected comment from the comments array
-	comments.splice(randomIndex, 1);
-
-	// Add the selected comment to the selected comments array
-	selectedComments.push(selectedComment);
-
-	// Log the selected comment
-	console.log(`Comment selected: ${selectedComment}`);
-
-	// Log the number of comments remaining in the comments array
-	console.log(`Comments left: ${comments.length}`);
-
-	// Log the number of comments in the selected comments array
-	console.log(`Selected comments: ${selectedComments.length}`);
-
-	// Return the selected comment
-	return selectedComment;
+	// check if the randomly selected comment is already in the array
+	if (selectedComments.includes(randomComment)) {
+		console.log(`Comment already selected, picking new comment`);
+		return pickRandomComment(user); // recursively call the function to select a new comment
+	}
+	// add the comment to the selectedComments array
+	selectedComments.push(randomComment);
+	console.log(`Number of comments stored: ${selectedComments.length}`);
+	return randomComment;
 };
 
 //exports
